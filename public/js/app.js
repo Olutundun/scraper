700ce1bb560c2e5c38adc2092048840c062f8be4
Your Name <tundun.deborah@gmail.com>
2019-05-28 19:52:00 -0400

    "saved articles works 90%"

--- public/js/app.js
+++ public/js/app.js
@@ -8,9 +8,9 @@ $(document).ready(function () {
         location.href = "http://localhost:3000"
     });
     //deletes ALL articles
-    $(".btn-outline-danger").on("click", deleteTask);
+    $(".btn-outline-danger").on("click", deleteArticles);
 
-    function deleteTask(event) {
+    function deleteArticles(event) {
         event.stopPropagation();
         $.ajax({
             method: "Delete",
@@ -18,8 +18,36 @@ $(document).ready(function () {
         }).then
         location.reload();
     };
-    //scrape new articles
-    $(".btn-outline-warning").on("click", function () {
-        location.href = "http://localhost:3000/scrape"
+    //scrape new articles 
+    $(".btn-outline-warning").on("click", scrapeArticle);
+
+    function scrapeArticle(event) {
+        event.stopPropagation();
+        $.ajax({
+            method: "GET",
+            url: "/scrape"
+        }).then(function (response) {
+            location.reload();
+        })
+    }
+
+    //save an article
+    $(".btn-info").on("click", function () {
+        let id = $(this).data("id");
+        let saved = $(this).data("saved");
+
+        let savedArticle = {
+            saved: saved
+        };
+        console.log(id)
+        console.log(savedArticle);
+        $.ajax("/saved" + id, {
+            method: "PUT",
+            saved: savedArticle
+        }).then(function () {
+            console.log("successfully saved this article!")
+        })
     });
+
+
 })
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,14 +5,16 @@ const cheerio = require("cheerio");
 module.exports = function (app) {
     //get all the articles from the db
     app.get("/", function (req, res) {
-        db.Article.find({}).then(function (dbArticle) {
+        db.Article.find({}).sort({
+            "_id": -1
+        }).then(function (dbArticle) {
             res.render("home", {
                 data: dbArticle
             });
         }).catch(function (err) {
             res.json(err)
-        })
-    })
+        });
+    });
     //get route for scraping the site
     app.get("/scrape", function (req, res) {
         axios.get("https://www.echojs.com/").then(function (response) {
@@ -20,20 +22,28 @@ module.exports = function (app) {
             const $ = cheerio.load(response.data);
 
             $("article h2").each(function (i, element) {
-                let result = {};
+                const result = {};
                 //add the text and href of every link and save them as properties of the result object
                 result.title = $(this).children("a").text();
                 result.link = $(this).children("a").attr("href");
-
-                //create a new article using the result object
-                db.Article.create(result).then(function (dbArticle) {
-                        //console.log(dbArticle)
-                    })
-                    .catch(function (err) {
-                        console.log(err) //add user validation here 
-                    })
+                //prevent duplicate entries
+                db.Article.findOne({
+                    title: result.title,
+                    link: result.link
+                }).then(function (dbArticle) {
+                    console.log(dbArticle.title + " already in db!")
+                }).catch(function (err) {
+                    //create a new article using the result object
+                    db.Article.create(result).then(function (dbArticle) {
+                            //console.log(dbArticle)
+                        })
+                        .catch(function (err) {
+                            console.log(err) //add user validation here 
+                        })
+                })
             })
             res.send("scrape complete")
+            res.end();
         });
     });
 
@@ -45,11 +55,14 @@ module.exports = function (app) {
         }).catch(function (err) {
             res.json(err);
         })
-    })
-    // Route for getting all Articles from the db
+    });
+
+    // Route for getting all saved articles
     app.get("/saved", function (req, res) {
-        //grab every dooc in the article collection
-        db.Article.find({}).then(function (dbArticle) {
+        //grab every doc in the article collection
+        db.Article.find({
+                saved: true
+            }).then(function (dbArticle) {
                 //send back to the client
                 res.render("saved", {
                     data: dbArticle
@@ -57,7 +70,45 @@ module.exports = function (app) {
             })
             .catch(function (err) {
                 return res.status(500).end();
+            });
+    })
+
+    //route for saving an article 
+    app.put("/saved:id", function (req, res) {
+        db.Article.findByIdAndUpdate({
+                _id: req.params.id
+            }, {
+                $set: {
+                    saved: true
+                }
+            })
+            .then(function (dbArticle) {
+                res.render("saved", {
+                    data: dbArticle
+                })
+                console.log(dbArticle)
+            }).catch(function (err) {
+                res.json(err)
             })
     })
 
-}
+
+    // Route for deleting/updating saved article
+    app.put("/delete/:id", function (req, res) {
+
+        db.Article
+            .findByIdAndUpdate({
+                _id: req.params.id
+            }, {
+                $set: {
+                    "saved": false
+                }
+            })
+            .then(function (dbArticle) {
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
+    });
+};
